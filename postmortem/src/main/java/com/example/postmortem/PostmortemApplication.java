cee96b9706e131c9be4135771840aa8b3f95b979
Ajit Mario John <ajitmariojohn@gmail.com>
2021-06-02 09:22:22 +0530

    creating project

---
+++ postmortem/src/main/java/com/example/postmortem/PostmortemApplication.java
@@ -0,0 +1,13 @@
+package com.example.postmortem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmortemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostmortemApplication.class, args);
+	}
+
+}
---
+++ postmortem/src/test/java/com/example/postmortem/PostmortemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postmortem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmortemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
